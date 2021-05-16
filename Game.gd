28eaa8de7954110b276aa8475ae62ff28a96d144
NoahJacobsen <noahedjacobsen@gmail.com>
2021-05-16 14:19:09 -0700

    Minor improvements to movement

--- Game.gd
+++ Game.gd
@@ -2,3 +2,4 @@ extends Node
 
 export (int) var x_clamp = 125
 export (int) var y_clamp = 150
+export (int) var y_clamp_margin = 25
--- Objects/Player.gd
+++ Objects/Player.gd
@@ -1,21 +1,22 @@
 extends KinematicBody2D
 
-const Y_CLAMP_MARGIN = 25
 
-export (int) var speed = 225
-export (int) var acceleration = 25
+export (int) var max_speed = 225
+export (int) var acceleration = 20
 
 onready var game_controller = get_tree().get_nodes_in_group("game_controller")[0]
 
 var screen_size
 var x_clamp
 var y_clamp
+var y_clamp_margin
 
 var current_speed = 0
 
 func _ready():
 	x_clamp = game_controller.x_clamp
 	y_clamp = game_controller.y_clamp
+	y_clamp_margin = game_controller.y_clamp_margin
 	screen_size = get_viewport_rect().size
 	position.x = x_clamp
 
@@ -26,18 +27,14 @@ func _process(delta):
 	if (Input.is_action_pressed("ui_down")):
 		direction.y += 1
 	if (direction.length() > 0):
-		if (current_speed < speed && current_speed > -speed):
+		if (current_speed < max_speed && current_speed > -max_speed):
 			current_speed += direction.y * acceleration
-		#direction = direction.normalized() * speed
-		# Animate sprite
 	else:
-		var swerving = false
 		if (current_speed > 0):
 			current_speed -= acceleration
 		if (current_speed < 0):
 			current_speed += acceleration
-			if swerving:
-				current_speed = 0
 	
 	position += Vector2(0, current_speed) * delta
-	position.y = clamp(position.y, y_clamp, screen_size.y - Y_CLAMP_MARGIN)
+	# NOTE: Clamping instead of decelerating might cause an animation problem
+	position.y = clamp(position.y, y_clamp, screen_size.y - y_clamp_margin)
